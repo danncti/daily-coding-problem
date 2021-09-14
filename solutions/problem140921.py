3505a961e62a42e30283355efa2e1e98ac162245
Daniel <danncti@gmail.com>
2021-09-14 21:49:05 +0100

    change fucntion condition and added few assert statements

--- solutions/problem140921.py
+++ solutions/problem140921.py
@@ -2,7 +2,7 @@
 
 def dcp(lst, number):
 
-    if len(lst)>2:
+    if len(lst)>=2:
         for value in lst[1::]:
             if lst[0] + value == number:
                 return True
@@ -10,8 +10,11 @@ def dcp(lst, number):
     return False
 
 def main():
-    test = [10, 15, 3, 7]
-    print(dcp(test, 17))
+
+    assert not dcp([], 17)
+    assert not dcp([4], 17)
+    assert dcp([17, 0], 17)
+    assert dcp([10, 15, 3, 7], 17)
 
 if __name__ == "__main__":
     main()
